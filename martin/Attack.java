633507f7f7068cb1854c9dcd4383fa0787592bb7
martin.dansc <martin.dansc@gmail.com>
2018-07-19 12:04:17 +0200

    martin package added

---
+++ martin/Attack.java
@@ -0,0 +1,171 @@
+package martin;
+
+import aic2018.*;
+
+public class Attack {
+
+    private MemoryManager manager;
+    private UnitController uc;
+    private Utils utils = new Utils();
+    private Pathfind pathfind;
+    Location target;
+
+    public Attack(MemoryManager memoryManager) {
+        this.manager = memoryManager;
+        uc = memoryManager.uc;
+        pathfind = new Pathfind(manager);
+        aggressive = false;
+
+        // choose randomly one objective
+        target = manager.startEnemies[(int)(Math.random()*manager.startEnemies.length)];
+    }
+
+    private Location myLocation;
+    private boolean aggressive;
+    private Location nextForTarget;
+
+    public void play() {
+
+        myLocation = manager.myLocation;
+
+        if(manager.getWarriorsNum() > 20) {
+            aggressive = true;
+        }
+
+        if(myLocation.distanceSquared(target) < 25) {
+            aggressive = false;
+        }
+
+        tryAttackBestUnit();
+        move();
+        tryAttackBestUnit();
+        tryAttackTree();
+    }
+
+    public void move() {
+        if(aggressive) {
+            Direction dir = pathfind.getNextLocationTarget(target);
+            if(dir != null) nextForTarget = myLocation.add(dir);
+        }
+        else {
+            nextForTarget = null;
+        }
+
+        Location newLoc = evalLocation();
+        if (!newLoc.equals(myLocation)) {
+            uc.move(myLocation.directionTo(newLoc));
+            manager.myLocation = newLoc;
+            myLocation = manager.myLocation;
+        }
+    }
+
+    public Location evalLocation() {
+
+        Location plocs[] = utils.getPosibleMoves(uc);
+        UnitInfo[] units = uc.senseUnits();
+        VictoryPointsInfo[] points = uc.senseVPs();
+        Team allies = uc.getTeam();
+
+        float highestValue = -100000;
+        Location bestLocation = myLocation;
+
+        for (int j = 0; j < plocs.length; j++) {
+            float value = 0;
+
+            int numWorkers = 0;
+
+            for (int i = 0; i < units.length; i++) {
+                UnitInfo currentUnit = units[i];
+                float distance = plocs[j].distanceSquared(currentUnit.getLocation());
+                Team unitTeam = currentUnit.getTeam();
+                UnitType unitType = currentUnit.getType();
+
+                if(unitTeam != allies) {
+                    if (unitType == UnitType.BARRACKS) {
+                        value += 8 / (1 + distance) - currentUnit.getHealth()/6;
+                    }
+                    else if(unitType == UnitType.WORKER) {
+                        value += 16 / (1 + distance) - currentUnit.getHealth()/6;
+                    }
+                    else {
+                        value += 100 / (1 + distance) - currentUnit.getHealth()/6;
+                    }
+                }
+                else if(unitType != UnitType.WORKER && unitType != UnitType.BARRACKS){
+                    if (distance <= 4) {
+                        value -= 4;
+                    } else if (distance < 10) {
+                        value -= 2;
+                    }
+                }
+                else {
+                    numWorkers++;
+                }
+
+                // stay at the front line
+                if(numWorkers > 3) {
+                    value -= 1;
+                }
+                else {
+                    value += 1;
+                }
+
+                if(nextForTarget != null && nextForTarget.isEqual(plocs[j])) {
+                    value += 3;
+                }
+
+            }
+
+            for (int i = 0; i < points.length; i++) {
+                VictoryPointsInfo currentVP = points[i];
+                float distance = plocs[j].distanceSquared(currentVP.getLocation());
+                value += 2 / (1 + distance);
+            }
+            if (highestValue < value) {
+                highestValue = value;
+                bestLocation = plocs[j];
+            }
+        }
+        return bestLocation;
+
+    }
+
+    public boolean tryAttackBestUnit() {
+        UnitInfo[] enemies = manager.enemies;
+        if(enemies.length == 0 || !uc.canAttack()) return false;
+
+        UnitInfo enemy = enemies[0];
+        int maxHealth = 10001;
+
+        for (UnitInfo unit : enemies){
+            int enemyHealth = unit.getHealth();
+            if (maxHealth > enemyHealth && uc.canAttack(unit)) {
+                maxHealth = enemyHealth;
+                enemy = unit;
+            }
+        }
+
+        if(maxHealth != 10001) {
+            uc.attack(enemy);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean tryAttackTree() {
+        TreeInfo[] trees = manager.trees;
+        if(trees.length == 0 || !uc.canAttack()) return false;
+
+        for (TreeInfo tree : trees){
+            if(tree.isOak() && uc.canAttack(tree)) {
+                uc.attack(tree);
+            }
+        }
+
+        return false;
+    }
+
+
+
+}
---
+++ martin/Utils.java
@@ -0,0 +1,108 @@
+package martin;
+
+import aic2018.*;
+
+public class Utils {
+
+    public void buyPointsIfNeeded(UnitController uc) {
+        if (uc.canBuyVP( GameConstants.VICTORY_POINTS_MILESTONE - uc.getTeam().getVictoryPoints())){
+            uc.buyVP(GameConstants.VICTORY_POINTS_MILESTONE - uc.getTeam().getVictoryPoints());
+        }
+
+        if (uc.getRound() == GameConstants.MAX_TURNS) {
+            uc.buyVP( uc.getResources()/(GameConstants.BASE_VICTORY_POINTS_COST +
+                    GameConstants.MAX_TURNS/GameConstants.VICTORY_POINTS_INFLATION_ROUNDS));
+        }
+    }
+
+    public void pickVictoryPoints(UnitController uc) {
+        Direction[] dirs = Direction.values();
+        for(int i = 0; i < 8; i++) {
+            if(uc.canGatherVPs(dirs[i])) {
+                uc.gatherVPs(dirs[i]);
+            }
+        }
+    }
+
+    public Location[] getPosibleMoves(UnitController uc) {
+
+        if (!uc.canMove()) {
+            Location locs[] = new Location[0];
+            return locs;
+        }
+
+        Location locs[] = new Location[9];
+        locs[8] = uc.getLocation();
+        Direction[] dirs = Direction.values();
+        int possibilities = 0;
+
+        for(int i = 0; i < 8; i++) {
+            Location next = locs[8].add(dirs[i]);
+            if(uc.isAccessible(next)) {
+                locs[i] = next;
+                possibilities++;
+            }
+        }
+
+        Location realLocs[] = new Location[possibilities + 1];
+        int counter = 0;
+        for (int i = 0; i < 9; i++) {
+            if (locs[i] != null) {
+                realLocs[i - counter] = locs[i];
+            } else {
+                counter++;
+            }
+        }
+
+        return realLocs;
+    }
+
+    public Location[] getLocations(UnitController uc, Location loc) {
+        Location locs[] = new Location[8];
+
+        Direction[] dirs = Direction.values();
+        for(int i = 0; i < 8; i++) {
+            locs[i] = (loc.add(dirs[i]));
+        }
+
+        return locs;
+    }
+
+    public Boolean isExtreme(UnitController uc, Location loc) {
+        Location[] newPlaces = getLocations(uc, loc);
+        for (int i = 0; i < newPlaces.length; i++) {
+            if (uc.canSenseLocation(newPlaces[i]) && uc.isOutOfMap(newPlaces[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean isWater(UnitController uc, Location loc) {
+        Location[] newPlaces = getLocations(uc, loc);
+        for (int i = 0; i < newPlaces.length; i++) {
+            if (uc.canSenseLocation(newPlaces[i]) && uc.senseWaterAtLocation(newPlaces[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean canPlantTree(MemoryManager manager) {
+        return manager.resources > 179 && manager.objective == UnitType.WORKER;
+    }
+
+    public Boolean canSpawnWorker(MemoryManager manager) {
+        return (manager.resources > 199 && manager.objective == UnitType.WORKER);
+    }
+
+    public Boolean canSpawnBarraks(UnitInfo unit, MemoryManager manager) {
+        return (((unit.getTeam() == manager.opponent && manager.getBarracksNum() < 6) ||
+                manager.getBarracksNum() < 1) && manager.resources > 499);
+    }
+
+    public Boolean canSpawnBarraks(MemoryManager manager) {
+        return manager.getBarracksNum() < 1 && manager.resources > 499;
+    }
+
+}
